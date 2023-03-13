23ad4adf9076ed679a9fe4c3f5d4cc5fa4a8a29d
swizz6ix <ferdinandcharles6@gmail.com>
2023-03-12 23:21:04 -0700

    air

--- console.py
+++ console.py
@@ -32,27 +32,24 @@ def parse(arg):
 
 
 class HBNBCommand(cmd.Cmd):
-             """Defines the HolbertonBnB command interpreter.
-                 Attributes:
-                     prompt (str): The command prompt.
-             """
-
-prompt = "(hbnb)"
-__classes = {
-    "BaseModel",
-    "User",
-    "State",
-    "City",
-    "Place",
-    "Amenity",
-    "Review"
-    }
-
-def emptyline(self):
+    """Defines the HolbertonBnB command interpreter.
+    Attributes:
+    prompt (str): The command prompt.
+    """
+    prompt = "(hbnb)"
+    __classes = {
+            "BaseModel",
+            "User",
+            "State6",
+            "City",
+            "Place",
+            "Amenity",
+            "Review"
+            }
+    def emptyline(self):
         """Do nothing upon receiving an empty line."""
         pass
-
-def default(self, arg):
+    def default(self, arg):
         """Default behavior for cmd module when input is invalid"""
         argdict = {
             "all": self.do_all,
@@ -73,16 +70,16 @@ def default(self, arg):
         print("*** Unknown syntax: {}".format(arg))
         return False
 
-def do_quit(self, arg):
+    def do_quit(self, arg):
         """Quit command to exit the program."""
         return True
 
-def do_EOF(self, arg):
+    def do_EOF(self, arg):
         """EOF signal to exit the program."""
         print("")
         return True
 
-def do_create(self, arg):
+    def do_create(self, arg):
         """Usage: create <class>
         Create a new class instance and print its id.
         """
@@ -95,7 +92,7 @@ def do_create(self, arg):
             print(eval(argl[0])().id)
             storage.save()
 
-def do_show(self, arg):
+    def do_show(self, arg):
         """Usage: show <class> <id> or <class>.show(<id>)
         Display the string representation of a class instance of a given id.
         """
@@ -112,7 +109,7 @@ def do_show(self, arg):
         else:
             print(objdict["{}.{}".format(argl[0], argl[1])])
 
-def do_destroy(self, arg):
+    def do_destroy(self, arg):
         """Usage: destroy <class> <id> or <class>.destroy(<id>)
         Delete a class instance of a given id."""
         argl = parse(arg)
@@ -129,23 +126,23 @@ def do_destroy(self, arg):
             del objdict["{}.{}".format(argl[0], argl[1])]
             storage.save()
 
-def do_all(self, arg):
+    def do_all(self, arg):
                 """Usage: all or all <class> or <class>.all()
                 Display string representations of all instances of a given class.
                 If no class is specified, displays all instantiated objects."""
-argl = parse(arg)
-if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
-        else:
-            objl = []
-            for obj in storage.all().values():
-                if len(argl) > 0 and argl[0] == obj.__class__.__name__:
-                    objl.append(obj.__str__())
-                elif len(argl) == 0:
-                    objl.append(obj.__str__())
-            print(objl)
+                argl = parse(arg)
+                if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
+                    print("** class doesn't exist **")
+                else:
+                    objl = []
+                    for obj in storage.all().values():
+                        if len(argl) > 0 and argl[0] == obj.__class__.__name__:
+                            objl.append(obj.__str__())
+                        elif len(argl) == 0:
+                            objl.append(obj.__str__())
+                            print(objl)
 
-def do_count(self, arg):
+    def do_count(self, arg):
         """Usage: count <class> or <class>.count()
         Retrieve the number of instances of a given class."""
         argl = parse(arg)
@@ -155,7 +152,7 @@ def do_count(self, arg):
                 count += 1
         print(count)
 
-def do_update(self, arg):
+    def do_update(self, arg):
         """Usage: update <class> <id> <attribute_name> <attribute_value> or
        <class>.update(<id>, <attribute_name>, <attribute_value>) or
        <class>.update(<id>, <dictionary>)
